c7d71df754c87c1c5bd6c7cce41004930d3a5588
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2021-12-01 16:51:23 -0300

    segudno commit

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ exports.__esModule = true;
 var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
-console.log(process.env);
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World!, how are you?');
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:".concat(port));

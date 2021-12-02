894dd4476fc9b3bc2a10d168f38dac87fa5637a7
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2021-12-01 21:45:58 -0300

    dev.ts creado

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import *as express from"express"
 const port = process.env.PORT || 3000
 const app = express();
 
+console.log(process.env.NODE_ENV);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!, how are you?, i am javi')
+    res.send({
+      enviromtent:process.env.NODE_ENV
+    })
   });
   
   app.listen(port, () => {

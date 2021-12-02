81faf535f5f67c7a96e0a15acf10bb59779c3a61
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2021-12-01 21:57:31 -0300

    dev

--- index.ts
+++ index.ts
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
       enviromtent:process.env.NODE_ENV
     })
   });
+  app.get('/db', (req, res) => {
+    res.send({
+      db:process.env.DB_HOST
+    })
+  });
   
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
